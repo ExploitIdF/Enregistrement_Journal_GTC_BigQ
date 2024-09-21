92c7984e9946b944088c50d0f88a8c5364f87eb3
ExploitIdF <141207765+ExploitIdF@users.noreply.github.com>
2024-09-21 09:48:02 +0200

    Aménagements mineurs

--- prog.py
+++ prog.py
@@ -1,6 +1,5 @@
 import pandas as pd, numpy as np
 import glob, re,datetime
-import plotly.graph_objects as go
 from google.cloud import bigquery
 from google.cloud import storage
 client = bigquery.Client()
@@ -9,7 +8,6 @@ clientST=storage.Client()
 blobs = clientST.list_blobs('gtc-test')
 for blob in blobs:
     print(blob.name)
-
 
 table_id='tunnels-dirif.GTC.Consignations'
 job_config = bigquery.LoadJobConfig(
@@ -26,11 +24,10 @@ job_config = bigquery.LoadJobConfig(
     skip_leading_rows=1,
     write_disposition="WRITE_TRUNCATE",
 )
-
 uri='https://storage.googleapis.com/gtc-test/consignations/Journal_2 (72).csv'
 load_job = client.load_table_from_uri(
     uri, table_id, job_config=job_config
 )   
 load_job.result()  
 destination_table = client.get_table(table_id)
-print("Loaded {} rows.".format(destination_table.num_rows))
+print("{} enregistrements envoyés".format(destination_table.num_rows))
